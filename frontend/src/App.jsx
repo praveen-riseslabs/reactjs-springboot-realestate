9325201a89870d18b2274e7ab5f94e72a0a22915
Ayushgokhe123 <ayush.gokhe@riseslabs.com>
2024-01-30 12:53:20 +0530

    login Folder Name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import Login from './Components/Login/Login'
+import Login from './Components/login/Login'
 import Registration from './Components/registration/Registration';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
