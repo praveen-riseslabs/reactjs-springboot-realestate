f9cea10878f9fbee864f6f51f26a21c3314c3049
KalyanMushnam <kalyan.mushnam@riseslabs.com>
2024-02-03 20:08:25 +0530

    Forgot password and Reset password

--- frontend/src/jsx/pages/Forgetpassword.js
+++ frontend/src/jsx/pages/Forgetpassword.js
@@ -13,11 +13,7 @@ export default function ForgetPassword() {
     setLoading(true);
 
     try {
-      let res = await axios.post(
-        'http://ec2-54-90-254-70.compute-1.amazonaws.com:8086/api/public/forgotpassword',
-        obj
-      );
-
+      let res = await axios.post('http://ec2-54-90-254-70.compute-1.amazonaws.com:8086/api/public/forgotpassword', obj );
       setLoading(false);
 
       if (res.status === 200) {
@@ -48,21 +44,12 @@ export default function ForgetPassword() {
           {message.length !== 0 && <h6 className='text-success text-center'>{message}</h6>}
           {err.length !== 0 && <h6 className='text-danger text-center'>{err}</h6>}
           <div class='col-md-12 '>
-            <label for='email' class='form-label'>
-              Email
-            </label>
-            <input
-              type='email'
-              class='form-control'
-              placeholder='Enter your mail to send Forget Password reset link'
-              {...register('email')}
-              id='email'
-            />
+            <label for='email' class='form-label'> Email </label>
+            <input type='email' class='form-control' placeholder='Enter your mail to send Forget Password reset link' {...register('email')} id='email'/>
           </div>
+
           <div class='col-md-4'>
-            <button id='btn' type='submit' class='btn btn-primary w-100'>
-              Send
-            </button>
+            <button id='btn' type='submit' class='btn btn-primary w-100'> Send </button>
           </div>
         </form>
       )}
