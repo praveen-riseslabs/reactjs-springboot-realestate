6c263f92bc75431622f8e3357de3a17c217f24a7
KalyanMushnam <kalyan.mushnam@riseslabs.com>
2024-02-05 13:54:43 +0530

    Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,106 +1,76 @@
 
-import { lazy, Suspense, useEffect, } from 'react';
-
-/// Components
-import Index from "./jsx";
+import React, { useEffect, Suspense } from 'react';
+import { Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { connect, useDispatch } from 'react-redux';
-import {  Route, Routes, useLocation , useNavigate , useParams } from 'react-router-dom';
-// action
 import { checkAutoLogin } from './services/AuthService';
 import { isAuthenticated } from './store/selectors/AuthSelectors';
-
-
+import Index from "./jsx";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-
 import "./css/style.css";
-import Dashboard from './jsx/pages/Dashboard';
 import ForgetPassword from './jsx/pages/Forgetpassword';
 import ResetPassword from './jsx/pages/Resetpassword';
+import AppProfile from './jsx/components/AppsMenu/AppProfile/AppProfile';
+import Inbox from './jsx/components/AppsMenu/Email/Inbox/Inbox';
 
-const SignUp = lazy(() => import('./jsx/pages/Registration'));
-const Login = lazy(() => {
-    return new Promise(resolve => {
-    setTimeout(() => resolve(import('./jsx/pages/Login')), 500);
-  });
-});
+const SignUp = React.lazy(() => import('./jsx/pages/Registration'));
+const Login = React.lazy(() => import('./jsx/pages/Login'));
 
-function withRouter(Component) {
-  function ComponentWithRouterProp(props) {
-    let location = useLocation();
-    let navigate = useNavigate();
-    let params = useParams();
-	
-    return (
-      <Component
-        {...props}
-        router={{ location, navigate, params }}
-      />
-    );
-  }
-
-  return ComponentWithRouterProp;
-}
-
-
-
-function App (props) {
+function App(props) {
     const dispatch = useDispatch();
     const navigate = useNavigate();    
-  const location = useLocation().pathname;
+    const location = useLocation().pathname;
 
-      useEffect(() => {
+    useEffect(() => {
         checkAutoLogin(dispatch, navigate, location);    
-      }, []);
+    }, []);
 
-    
     let routeblog = (         
       <Routes>   
-        <Route  path='/login' element={<Login />} />
-        <Route path='/register' element={<SignUp />} />        
-        <Route path='/dashboard' element={<Dashboard />} />        
-        <Route path='/forgetpassword' element={<ForgetPassword />} />   
-        <Route path='/reset_password' element={<ResetPassword />} />      
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<SignUp />} />
+        <Route path='/dashboard/*' element={<Index />} />
+        <Route path='/forgetpassword' element={<ForgetPassword />} />
+        <Route path='/reset_password' element={<ResetPassword />} />
+        <Route path='/app-profile' element={<AppProfile />} />
+        <Route path='/email-inbox' element={<Inbox />} />
       </Routes> 
     );
+
     if (props.isAuthenticated) {
-		  return (
-			  <>
-            <Suspense fallback={              
-                <div id="preloader">                
-                    <div className="sk-three-bounce">
-                        <div className="sk-child sk-bounce1"></div>
-                        <div className="sk-child sk-bounce2"></div>
-                        <div className="sk-child sk-bounce3"></div>
-                    </div>
-                </div>  
-              }
-            >
-              <Index /> 
-            </Suspense>
+      return (
+        <>
+          <Suspense fallback={              
+            <div id="preloader">                
+              <div className="sk-three-bounce">
+                <div className="sk-child sk-bounce1"></div>
+                <div className="sk-child sk-bounce2"></div>
+                <div className="sk-child sk-bounce3"></div>
+              </div>
+            </div>  
+          }>
+            <Index /> 
+          </Suspense>
         </>
       );    
-	}else{
-		return (
-			  <div className="vh-100">
-            <Suspense fallback={
-                <div id="preloader">
-                    <div className="sk-three-bounce">
-                        <div className="sk-child sk-bounce1"></div>
-                        <div className="sk-child sk-bounce2"></div>
-                        <div className="sk-child sk-bounce3"></div>
-                    </div>
-                </div>
-              }
-            >
-                {routeblog}
-                
-            </Suspense>
-			  </div>
-		);
-	}
-};
-
+    } else {
+      return (
+        <div className="vh-100">
+          <Suspense fallback={
+            <div id="preloader">
+              <div className="sk-three-bounce">
+                <div className="sk-child sk-bounce1"></div>
+                <div className="sk-child sk-bounce2"></div>
+                <div className="sk-child sk-bounce3"></div>
+              </div>
+            </div>
+          }>
+            {routeblog}
+          </Suspense>
+        </div>
+      );
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -108,5 +78,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default withRouter(connect(mapStateToProps)(App)); 
-
+export default connect(mapStateToProps)(App);
