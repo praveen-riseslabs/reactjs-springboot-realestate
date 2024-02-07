08027d691e117a3514168cfa2c903093a3a1f4c3
AjayGowlikar <152581936+AjayGowlikar@users.noreply.github.com>
2024-02-07 13:29:56 +0530

    Update RegistrationController.java to send message on successfully Registration

--- backend/src/main/java/com/findprecon/controller/PublicApis/RegistrationController.java
+++ backend/src/main/java/com/findprecon/controller/PublicApis/RegistrationController.java
@@ -7,13 +7,19 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @CrossOrigin
@@ -25,6 +31,10 @@ public class RegistrationController {
 	@Autowired
 	private Service service;
 
+	@Autowired
+	private JavaMailSender mailSender;
+
+
 	@PostMapping("/register")
 	@Operation(summary = "create new user !!, this is registration api !!")
 	@ApiResponses(value ={
@@ -32,14 +42,31 @@ public class RegistrationController {
 			@ApiResponse(responseCode = "401", description = "not authorized"),
 			@ApiResponse(responseCode = "201", description = "new user created")
 	})
-	public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterDTO registerDTO){
+	public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterDTO registerDTO) throws MessagingException, UnsupportedEncodingException {
 		RegistrationModel createdUser =  service.registerUser(registerDTO);
 		if (createdUser == null){
 			return new ResponseEntity<>("User not created, come again later!", HttpStatus.BAD_REQUEST);
 		}
-		return new ResponseEntity<>("Hey "+ createdUser.getName() + " Registration successful with email " + createdUser.getEmail(), HttpStatus.CREATED);
+
+		sendEmail(createdUser.getEmail());
+		return new ResponseEntity<>("Hey "+ createdUser.getName() + " Resistration successful with email " + createdUser.getEmail(), HttpStatus.CREATED);
 
 	}
-	
+
+	private void sendEmail(String email) throws UnsupportedEncodingException, MessagingException {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+
+		helper.setFrom("AjayGowlikar@gmail.com", "Support");
+		helper.setTo(email);
+
+		String subject = "Welcome to FPC Organization";
+		String content = "<p>Hello,</p>" +
+				"<p>You have successfully registered with FPC Organization.</p>" +
+				"<p>Welcome aboard!</p>" ;
+		helper.setSubject(subject);
+		helper.setText(content, true);
+		mailSender.send(message);
+	}
 
 }
