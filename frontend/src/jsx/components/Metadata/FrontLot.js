3bfc4036fc14aea643815107e84ae754eedfdc64
KalyanMushnam <kalyan.mushnam@riseslabs.com>
2024-02-16 19:09:02 +0530

    metadata forms

--- frontend/src/jsx/components/Metadata/FrontLot.js
+++ frontend/src/jsx/components/Metadata/FrontLot.js
@@ -1,32 +1,86 @@
-import React, { useState } from 'react';
-// import './style.scss'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function FrontLot() {
-  const [metadata, setMetadata] = useState([
-    { id: 1, type: 'Type 1' },
-    { id: 2, type: 'Type 2' },
-    { id: 3, type: 'Type 3' }
-  ]);
-  const [editingIndex, setEditingIndex] = useState(-1); // Initially no row is being edited
+  const [metadata, setMetadata] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(-1);
+  const [newType, setNewType] = useState(false);
+
+
+  const getvalues = () => {
+    axios.get('http://localhost:8086/api/metadata/frontlot/all')
+    .then(response => {
+      setMetadata(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  }
+
+  useEffect(() => {
+    getvalues()
+  }, []);
 
   const handleAddRow = () => {
-    setMetadata([...metadata, { id: metadata.length + 1, type: '' }]);
+    const newMetadata = [...metadata, { type: '' }];
+    setMetadata(newMetadata);
+    setEditingIndex(newMetadata.length - 1);
+    setNewType(true)
   };
 
+  const handleNewData = (index) => {
+    const newData = {
+      frontLot: metadata[index].type // Only send the 'type' field to the backend
+    };
+    
+    axios.post('http://localhost:8086/api/metadata/frontlot/add', newData)
+      .then(response => {
+        console.log('Data saved successfully:', response.data);
+        setNewType(false);
+        setEditingIndex(-1);
+        getvalues()
+      })
+      .catch(error => {
+        console.error('Error saving data:', error);
+        getvalues()
+      });
+  };
+
+
   const handleEdit = (index) => {
-    // Set the editingIndex to the index of the row being edited
     setEditingIndex(index);
   };
 
   const handleSave = (index) => {
-    // Reset editingIndex when "Save" button is clicked
-    setEditingIndex(-1);
+    const updatedData = {
+      id: metadata[index].id,
+      frontLot: metadata[index].type
+    };
+  
+    axios.put(`http://localhost:8086/api/metadata/frontlot/update/${updatedData.id}`, updatedData)
+      .then(response => {
+        console.log('Data updated successfully:', response.data);
+        setEditingIndex(-1);
+        getvalues()
+      })
+      .catch(error => {
+        console.error('Error updating data:', error);
+      });
   };
+  
 
   const handleDelete = (index) => {
-    // Delete the item and update IDs of remaining items
-    const updatedMetadata = metadata.filter((item, i) => i !== index).map((item, i) => ({ ...item, id: i + 1 }));
-    setMetadata(updatedMetadata);
+    const garageIdToDelete = metadata[index].id; 
+    axios.delete(`http://localhost:8086/api/metadata/frontlot/delete/${garageIdToDelete}`)
+      .then(response => {
+        console.log('Data deleted successfully:', response.data);
+        
+        const updatedMetadata = metadata.filter((item, i) => i !== index);
+        setMetadata(updatedMetadata);
+      })
+      .catch(error => {
+        console.error('Error deleting data:', error);
+      });
   };
 
   const handleChange = (index, field, e) => {
@@ -41,24 +95,25 @@ function FrontLot() {
       <table className="table">
         <thead>
           <tr>
-            <th className='w-10 text-center'>ID</th>
-            <th className='text-center w-'>FrontLot</th>
+            <th className='w-10 text-center'>S.No</th>
+            <th className='text-center w-40'>Front Lot</th>
             <th className='w-50'>Action</th>
           </tr>
         </thead>
         <tbody>
           {metadata.map((item, index) => (
             <tr key={index}>
-              <td className='w-10 text-center'>{item.id}</td>
+              <td className='w-10 text-center'>{index + 1}</td>
               <td className='text-center w-40 '>
                 {editingIndex === index ? (
                   <input type="text" value={item.type} onChange={(e) => handleChange(index, 'type', e)} className="form-control text-center"   />
-                  ) : (
-                  item.type
-                  )}
+                ) : (
+                  item.frontLot
+                )}
               </td>
               <td className='w-50'>
                 {editingIndex === index ? (
+                  newType ? <button  className="btn btn-success" onClick={() => handleNewData(index)}>Add Data</button> :
                   <button className="btn btn-success" onClick={() => handleSave(index)}>Save</button>
                 ) : (
                   <>
@@ -74,7 +129,5 @@ function FrontLot() {
     </div>
   );
 }
-
-
 
 export default FrontLot
--- frontend/src/jsx/components/Metadata/PropertyType.js
+++ frontend/src/jsx/components/Metadata/PropertyType.js
@@ -1,31 +1,86 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function PropertyType() {
-  const [metadata, setMetadata] = useState([
-    { id: 1, type: 'Type 1' },
-    { id: 2, type: 'Type 2' },
-    { id: 3, type: 'Type 3' }
-  ]);
+  const [metadata, setMetadata] = useState([]);
   const [editingIndex, setEditingIndex] = useState(-1);
-  const [newType, setNewType] = useState('');
+  const [newType, setNewType] = useState(false);
+
+
+  const getvalues = () => {
+    axios.get('http://localhost:8086/api/metadata/property/all')
+    .then(response => {
+      setMetadata(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  }
+
+  useEffect(() => {
+    getvalues()
+  }, []);
 
   const handleAddRow = () => {
-    const newMetadata = [...metadata, { id: metadata.length + 1, type: '' }];
+    const newMetadata = [...metadata, { type: '' }];
     setMetadata(newMetadata);
-    setEditingIndex(newMetadata.length - 1); // Start editing the newly added row
+    setEditingIndex(newMetadata.length - 1);
+    setNewType(true)
   };
+
+  const handleNewData = (index) => {
+    const newData = {
+      propertyField: metadata[index].type // Only send the 'type' field to the backend
+    };
+    
+    axios.post('http://localhost:8086/api/metadata/property/add', newData)
+      .then(response => {
+        console.log('Data saved successfully:', response.data);
+        setNewType(false);
+        setEditingIndex(-1);
+        getvalues()
+      })
+      .catch(error => {
+        console.error('Error saving data:', error);
+        getvalues()
+      });
+  };
+
 
   const handleEdit = (index) => {
     setEditingIndex(index);
   };
 
   const handleSave = (index) => {
-    setEditingIndex(-1);
+    const updatedData = {
+      id: metadata[index].id,
+      propertyField: metadata[index].type
+    };
+  
+    axios.put(`http://localhost:8086/api/metadata/property/update/${updatedData.id}`, updatedData)
+      .then(response => {
+        console.log('Data updated successfully:', response.data);
+        setEditingIndex(-1);
+        getvalues()
+      })
+      .catch(error => {
+        console.error('Error updating data:', error);
+      });
   };
+  
 
   const handleDelete = (index) => {
-    const updatedMetadata = metadata.filter((item, i) => i !== index).map((item, i) => ({ ...item, id: i + 1 }));
-    setMetadata(updatedMetadata);
+    const garageIdToDelete = metadata[index].id; 
+    axios.delete(`http://localhost:8086/api/metadata/property/delete/${garageIdToDelete}`)
+      .then(response => {
+        console.log('Data deleted successfully:', response.data);
+        
+        const updatedMetadata = metadata.filter((item, i) => i !== index);
+        setMetadata(updatedMetadata);
+      })
+      .catch(error => {
+        console.error('Error deleting data:', error);
+      });
   };
 
   const handleChange = (index, field, e) => {
@@ -40,24 +95,25 @@ function PropertyType() {
       <table className="table">
         <thead>
           <tr>
-            <th className='w-10 text-center'>ID</th>
-            <th className='text-center w-40'>Type</th>
+            <th className='w-10 text-center'>S.No</th>
+            <th className='text-center w-40'>Property Type</th>
             <th className='w-50'>Action</th>
           </tr>
         </thead>
         <tbody>
           {metadata.map((item, index) => (
             <tr key={index}>
-              <td className='w-10 text-center'>{item.id}</td>
+              <td className='w-10 text-center'>{index + 1}</td>
               <td className='text-center w-40 '>
                 {editingIndex === index ? (
                   <input type="text" value={item.type} onChange={(e) => handleChange(index, 'type', e)} className="form-control text-center"   />
                 ) : (
-                  item.type
+                  item.propertyField
                 )}
               </td>
               <td className='w-50'>
                 {editingIndex === index ? (
+                  newType ? <button  className="btn btn-success" onClick={() => handleNewData(index)}>Add Data</button> :
                   <button className="btn btn-success" onClick={() => handleSave(index)}>Save</button>
                 ) : (
                   <>
